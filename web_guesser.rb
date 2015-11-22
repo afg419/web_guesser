a6fe4ad5c60e4f4d8dc76caf4f84419c0430fb77
Aaron Greenspan <afg419@gmail.com>
2015-11-22 11:12:50 -0700

    can guess n stuff

--- web_guesser.rb
+++ web_guesser.rb
@@ -2,7 +2,20 @@ require 'sinatra'
 require 'sinatra/reloader'
 
 n = rand(100)
+m = "Let's play!"
+g = "awaiting first guess"
 
 get '/' do
-  erb :index, :locals => {:n => n}
+  if params["guess"]
+    g = params["guess"].to_i
+    m = "Too High!" if g > n
+  end
+  erb :index, :locals => {:n => n, :m => m, :g => g}
 end
+
+
+
+# if the guess is too high
+#   set the message to "Too high!"
+# end
+# render the ERB template and pass in the number AND the message
