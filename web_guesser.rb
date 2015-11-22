a338f521d9aa4b72638726f0087cce30d0550463
Aaron Greenspan <afg419@gmail.com>
2015-11-22 11:28:41 -0700

    using cond hash

--- web_guesser.rb
+++ web_guesser.rb
@@ -5,10 +5,18 @@ n = rand(100)
 m = "Let's play!"
 g = "awaiting first guess"
 
+def cond_hash
+  {
+    -1 => {true => "Way too low!", false => "Close, but too low!"},
+    0 => {false => "Nailed it!"},
+    1 => {true => "Way too high!", false => "Close, but too high!"}
+  }
+end
+
 get '/' do
   if params["guess"]
     g = params["guess"].to_i
-    m = "Too High!" if g > n
+    m = cond_hash[g <=> n][g < n ? g < n - 5 : g > n + 5]
   end
   erb :index, :locals => {:n => n, :m => m, :g => g}
 end
