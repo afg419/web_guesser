717e2a7422add0f50c4b79fccf053b78a4efe684
Aaron Greenspan <afg419@gmail.com>
2015-11-22 12:29:01 -0700

    two extensions

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,32 +1,46 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-SECRET_NUMBER = rand(10)
+@@guesses = 0
+
+@@secret = rand(10)
 m = "Let's play!"
 g = "awaiting first guess"
 
 def cond_hash
   {
-    -1 => {true => "Way too low!", false => "Close, but too low!"},
-    0 => {false => "Nailed it!"},
-    1 => {true => "Way too high!", false => "Close, but too high!"}
+    true => {true => "Way too low!", false => "Close, but too low!"},
+    false => {true => "Way too high!", false => "Close, but too high!"}
   }
 end
 
 def check_guess(guess)
   unless guess.nil?
-    cond_hash[guess <=> SECRET_NUMBER][guess < SECRET_NUMBER ? guess < SECRET_NUMBER - 5 : guess > SECRET_NUMBER + 5]
+    cond_hash[guess < @@secret][guess < @@secret ? guess < @@secret - 5 : guess > @@secret + 5]
   else
     "Let's play!"
   end
+end
+
+def restart
+  @@secret = rand(10)
+  @@guesses = 0
 end
 
 get '/' do
     g = params["guess"]
     c = params["cheat"]
     g = g.to_i unless g.nil?
+    @@guesses += 1 unless g.nil?
     m = check_guess(g)
-    erb :index, :locals => {:n => SECRET_NUMBER, :m => m, :g => g, :c => c}
+    if @@secret == g
+      m = "You win!!! We've RESET the number"
+      restart
+    elsif @@guesses > 4
+      m = "You lose!!! We've RESET the number"
+      restart
+    end
+    erb :index, :locals => {:n => @@secret, :m => m, :g => g, :c => c, :t => @@guesses}
 end
 
 
